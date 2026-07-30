392756c315ab4e6203ad33b684da386beba89f71
Sarvesh Asatkar <sarveshh.asatkarr@gmail.com>
2026-07-30 09:46:39 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "TituStoreBot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+def keep_alive():
+    Thread(target=run, daemon=True).start()
